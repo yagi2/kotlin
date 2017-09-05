7311207c8fa61464ba5816aa940f2b4e0744c6bb
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2017-09-05 13:23:52 +0200

    Destroy state and environment in android test

--- compiler/tests-common/org/jetbrains/kotlin/test/testFramework/KtUsefulTestCase.java
+++ compiler/tests-common/org/jetbrains/kotlin/test/testFramework/KtUsefulTestCase.java
@@ -68,16 +68,7 @@ public abstract class KtUsefulTestCase extends TestCase {
     }
 
     @NotNull
-    protected final Disposable myTestRootDisposable = new Disposable() {
-        @Override
-        public void dispose() { }
-
-        @Override
-        public String toString() {
-            String testName = getTestName(false);
-            return KtUsefulTestCase.this.getClass() + (StringUtil.isEmpty(testName) ? "" : ".test" + testName);
-        }
-    };
+    protected final Disposable myTestRootDisposable = new TestDisposable();
 
     private static final String ourPathToKeep = null;
     private final List<String> myPathsToKeep = new ArrayList<>();
@@ -489,4 +480,17 @@ public abstract class KtUsefulTestCase extends TestCase {
     private static boolean containsStressWords(@Nullable String name) {
         return name != null && (name.contains("Stress") || name.contains("Slow"));
     }
+
+
+    public class TestDisposable implements Disposable {
+        @Override
+        public void dispose() {
+        }
+
+        @Override
+        public String toString() {
+            String testName = getTestName(false);
+            return KtUsefulTestCase.this.getClass() + (StringUtil.isEmpty(testName) ? "" : ".test" + testName);
+        }
+    };
 }
