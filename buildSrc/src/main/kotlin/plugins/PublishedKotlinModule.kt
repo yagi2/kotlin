52c17a499deebae1c8189009bd9ecb55338a49e7
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2017-09-09 15:42:02 +0300

    Fix install task config (now for real)

--- buildSrc/src/main/kotlin/plugins/PublishedKotlinModule.kt
+++ buildSrc/src/main/kotlin/plugins/PublishedKotlinModule.kt
@@ -116,7 +116,7 @@ open class PublishedKotlinModule : Plugin<Project> {
                 }
             }
 
-            val install = if (tasks.names.contains("install")) tasks.getByName("uploadArchives") as Upload
+            val install = if (tasks.names.contains("install")) tasks.getByName("install") as Upload
                           else tasks.create("install", Upload::class.java)
             install.apply {
                 configuration = project.configurations.getByName(Dependency.ARCHIVES_CONFIGURATION)
