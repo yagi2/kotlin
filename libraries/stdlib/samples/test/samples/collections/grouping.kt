de8f0831ef90e5865047fff2699a056c0d9d4028
Itsuki Aoyagi <mail@yagi2.com>
2018-02-26 14:15:57 +0900

    [WIP] add sample for Grouping

---
+++ libraries/stdlib/samples/test/samples/collections/grouping.kt
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package samples.collections
+
+import samples.*
+import kotlin.test.*
+
+class Grouping {
+    @Sample
+    fun aggregateEvenAndOdd() {
+        val intList = listOf(0, 1, 2, 3, 4, 5, 6)
+
+        // to multiply even number by 10
+        val aggregated = intList.groupingBy { it % 2 == 0 }.aggregate { key, acc: Int?, element, _ ->
+            when (key) {
+                true -> element * 10 + (acc ?: 0)
+                else -> element + (acc ?: 0)
+            }
+        }
+
+        assertPrints(aggregated, "{true=120, false=9}")
+    }
+
+    @Sample
+    fun aggregateEvenAndOddToMap() {
+        val intList = listOf(0, 1, 2, 3, 4, 5, 6)
+        val aggregated = mutableMapOf<Boolean, Int>()
+
+        // to multiply even number by 10
+        intList.groupingBy { it % 2 == 0 }.aggregateTo(aggregated, { key, acc: Int?, element, _ ->
+            when (key) {
+                true -> element * 10 + (acc ?: 0)
+                else -> element + (acc ?: 0)
+            }
+        })
+
+        assertTrue(aggregated[true] == 120)
+        assertTrue(aggregated[false] == 9)
+    }
+
+    @Sample
+    fun foldWithComputedInitialValue() {
+
+    }
+
+    @Sample
+    fun foldWithComputedInitialValueToMap() {
+
+    }
+
+    @Sample
+    fun foldWithConstantInitialValue() {
+        val fruits = listOf("apple", "apricot", "banana", "blueberry", "cherry", "coconut")
+
+        // collect only even length Strings
+        val evenFruits = fruits.groupingBy { it.first() }
+            .fold(listOf<String>()) { acc, e -> if (e.length % 2 == 0) acc + e else acc }
+
+        assertPrints(evenFruits, "{a=[], b=[banana], c=[cherry]}")
+    }
+
+    @Sample
+    fun foldWithConstantInitialValueToMap() {
+        val fruits = listOf("apple", "apricot", "banana", "blueberry", "cherry", "coconut")
+        val evenFruits = mutableMapOf<Char, List<String>>()
+
+        // collect only even length Strings
+        fruits.groupingBy { it.first() }
+            .foldTo(evenFruits ,listOf()) { acc, e -> if (e.length % 2 == 0) acc + e else acc }
+
+        assertTrue(evenFruits['a'] == emptyList<String>())
+        assertTrue(evenFruits['b'] == listOf("banana"))
+        assertTrue(evenFruits['c'] == listOf("cherry"))
+    }
+
+    @Sample
+    fun reduceByMaxOfContainsVowels() {
+        val animals = listOf("raccoon", "reindeer", "cow", "camel", "giraffe", "goat")
+
+        // grouping by fisrt char and collect only max of contains vowels
+        val maxVowels = animals.groupingBy { it.first() }
+            .reduce { _, a, b ->
+                if (a.count{ it in "aeiou" } >= b.count{ it in "aeiou" }) {
+                    a
+                } else {
+                    b
+                }
+            }
+
+        assertPrints(maxVowels, "{r=reindeer, c=camel, g=giraffe}")
+    }
+
+    @Sample
+    fun reduceByMaxOfContainsVowelsToMap() {
+        val animals = listOf("raccoon", "reindeer", "cow", "camel", "giraffe", "goat")
+        val maxVowels = mutableMapOf<Char, String>()
+
+        // grouping by fisrt char and collect only max of contains vowels
+        animals.groupingBy { it.first() }
+            .reduceTo(maxVowels, { _, a, b ->
+                if (a.count{ it in "aeiou" } >= b.count{ it in "aeiou" }) {
+                    a
+                } else {
+                    b
+                }
+            })
+
+        assertTrue(maxVowels['r'] == "reindeer")
+        assertTrue(maxVowels['c'] == "camel")
+        assertTrue(maxVowels['g'] == "giraffe")
+    }
+}
--- libraries/stdlib/src/kotlin/collections/Grouping.kt
+++ libraries/stdlib/src/kotlin/collections/Grouping.kt
@@ -36,6 +36,7 @@ public interface Grouping<T, out K> {
  *  - `first`: indicates whether it's the first `element` encountered in the group.
  *
  * @return a [Map] associating the key of each group with the result of aggregation of the group elements.
+ * @sample samples.collections.Grouping.aggregateEvenAndOdd
  */
 @SinceKotlin("1.1")
 public inline fun <T, K, R> Grouping<T, K>.aggregate(
@@ -61,6 +62,7 @@ public inline fun <T, K, R> Grouping<T, K>.aggregate(
  * then the elements being aggregated for that key are never considered as `first`.
  *
  * @return the [destination] map associating the key of each group with the result of aggregation of the group elements.
+ * @sample samples.collections.Grouping.aggregateEvenAndOddToMap
  */
 @SinceKotlin("1.1")
 public inline fun <T, K, R, M : MutableMap<in K, R>> Grouping<T, K>.aggregateTo(
@@ -141,6 +143,7 @@ public inline fun <T, K, R, M : MutableMap<in K, R>> Grouping<T, K>.foldTo(
  *  - `element`: the element from the source being accumulated.
  *
  * @return a [Map] associating the key of each group with the result of accumulating the group elements.
+ * @sample samples.collections.Grouping.foldWithConstantInitialValue
  */
 @SinceKotlin("1.1")
 public inline fun <T, K, R> Grouping<T, K>.fold(
@@ -164,6 +167,7 @@ public inline fun <T, K, R> Grouping<T, K>.fold(
  *  - `element`: the element from the source being accumulated.
  *
  * @return the [destination] map associating the key of each group with the result of accumulating the group elements.
+ * @sample samples.collections.Grouping.foldWithConstantInitialValueToMap
  */
 @SinceKotlin("1.1")
 public inline fun <T, K, R, M : MutableMap<in K, R>> Grouping<T, K>.foldTo(
@@ -188,6 +192,7 @@ public inline fun <T, K, R, M : MutableMap<in K, R>> Grouping<T, K>.foldTo(
  *  - `element`: the element from the source being accumulated.
  *
  * @return a [Map] associating the key of each group with the result of accumulating the group elements.
+ * @sample samples.collections.Grouping.reduceByMaxOfContainsVowels
  */
 @SinceKotlin("1.1")
 public inline fun <S, T : S, K> Grouping<T, K>.reduce(
@@ -214,6 +219,7 @@ public inline fun <S, T : S, K> Grouping<T, K>.reduce(
  *  - `element`: the element from the source being folded;
  *
  * @return the [destination] map associating the key of each group with the result of accumulating the group elements.
+ * @sample samples.collections.Grouping.reduceByMaxOfContainsVowelsToMap
  */
 @SinceKotlin("1.1")
 public inline fun <S, T : S, K, M : MutableMap<in K, S>> Grouping<T, K>.reduceTo(
